7633550a5567333a98254dbb3640aac2f6056880
MRuniversee <renatobylyk@gmail.com>
2017-05-20 22:32:29 +0300

    New

--- Web-Server/js/message.js
+++ Web-Server/js/message.js
@@ -1,43 +1,15 @@
-var client = new Messaging.Client("test.mosca.io", 80, "arduino_" + parseInt(Math.random() * 100, 10));
-var onoff = 3;
+var client = new Messaging.Client("MQTT BROKER", 80, "NAME_" + parseInt(Math.random() * 100, 10));
 
 //"arduino_" + parseInt(Math.random() * 100, 10)
 //Gets  called if the websocket/mqtt connection gets disconnected for any reason
 client.onConnectionLost = function (responseObject) {
     //Depending on your scenario you could implement a reconnect logic here
-	window.location.replace("http://renatobylyk.com/projects/AC_project/index.php");
-	
+
 };
 //Gets called whenever you receive a message for your subscriptions
 function connectRoom(site_publish, callback) {
     client.onMessageArrived = function (message) {
-        if(!isNaN(message.payloadString)){
-			if (message.destinationName == site_publish + "/response/dht_hum") {
-				document.getElementById("dht_hum").innerHTML = message.payloadString + " %";
-			}
-			if (message.destinationName == site_publish + "/response/dht_temp") {
-				document.getElementById("dht_temp").innerHTML = message.payloadString + "°C";
-			}
-        }
-        if (message.destinationName == site_publish + "/response/watt") {
-            if (parseInt(message.payloadString) > 1000) {
-                document.getElementById("watt").innerHTML = "680.0";
-                if (parseInt(message.payloadString) > 110) {
-                    onoff = 1;
-                    openclose = 1;
-                }
-            } else {
-                document.getElementById("watt").innerHTML = message.payloadString;
-            }
-        }
-        if (message.destinationName == site_publish + '/response') {
-            if (message.payloadString == "connected") {
-                isConnected = 1;
-                callback(message.destinationName);
-            } else {
-                callback(message.payloadString);
-            }
-        }
+       // Your CALL BACK MESSAGE
     };
 }
 
@@ -51,7 +23,6 @@ var options = {
     ,
     //Gets Called if the connection could not be established
     onFailure: function (message) {
-        window.location.replace("http://renatobylyk.com/projects/AC_project/index.php");
     }
 };
 //Creates a new Messaging.Message Object and sends it to the HiveMQ MQTT Broker
@@ -63,4 +34,3 @@ var publish = function (payload, topic, qos) {
     message.qos = qos;
     client.send(message);
 }
-
